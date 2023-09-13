f247527e87dc6e02448250edab805742713156ef
3DRX <kjy20030228@icloud.com>
2023-09-13 14:27:20 +0800

    update motion

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ if __name__ == "__main__":
     while True:
         print("Pressing W")
         k.press("W")
-        time.sleep(5)
+        time.sleep(2)
         k.release("W")
-        print("Pressing S")
+        print("Pressing S and W")
         k.press("S")
-        time.sleep(5)
+        k.press("W")
+        time.sleep(2)
         k.release("S")
+        k.release("W")
         pass
     pass
